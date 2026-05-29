36c59a06edb8c4118d682b1a2fc19decca683105
Elusai Soares <elusaisoares@gmail.com>
2026-05-29 19:00:29 -0300

    feat: use database and env configs

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-const express = require('express')
-const booksRouter = require('./routes/books')
-const favoritesRouter = require('./routes/favorites')
+import express from 'express'
+import booksRouter from './routes/books.js'
+import favoritesRouter from './routes/favorites.js'
+import pool from './config/db-connection.js'
+import { PORT } from './config/environment-variables.js'
 
 const app = express()
 
-app.use(express.json())
+try {  
+    app.use(express.json())
+    app.use('/livros', booksRouter)
+    app.use('/favoritos', favoritesRouter)
 
-const PORT = 8000
+    const dbConnection = await pool()
+    console.log('Conexão com o banco de dados estabelecida com sucesso!')
+    dbConnection.on('error', (error) => console.error('Erro na conexão com o banco de dados:', error))    
+    app.listen(PORT, () => {
+        console.log(`Servidor rodando na porta ${PORT}`)
+    })
+} catch (error) {
+    console.error('Erro ao iniciar o servidor:', error)
+}
 
-app.use('/livros', booksRouter)
-app.use('/favoritos', favoritesRouter)
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`)
-})
