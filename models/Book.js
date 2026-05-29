75efbd1255655b9ee2ebf77ddce779f967f02612
Elusai Soares <elusaisoares@gmail.com>
2026-05-29 19:14:12 -0300

    fix: remove id

--- models/Book.js
+++ models/Book.js
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 
 const BookSchema = new mongoose.Schema({
-    id: { type: String, required: true },
     titulo: { type: String, required: true },
     autor: { type: String, required: true },
     ano_publicacao: { type: Number, required: true },
